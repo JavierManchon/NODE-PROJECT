aef178ec301d69e68da7385c073af1aa8c42a229
JavierManchon <javiermanchongarcia@gmail.com>
2023-12-07 11:26:42 +0100

    Styles added

--- frontend/script.js
+++ frontend/script.js
@@ -7,19 +7,19 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 async function fetchVideogames() {
-    const response = await fetch('http://localhost:3000/videogames'); // Ajusta esta ruta a tu API
+    const response = await fetch('http://localhost:3000/videogames'); 
     const videogames = await response.json();
     const videogamesList = document.getElementById('videogamesList');
     videogamesList.innerHTML = '';
     console.log(videogames)
     videogames.forEach(videogame => {
         const videogameCard = `<div class="videogameCard">
-                                <h3>${videogame.name}</h3>
+                                <h3 class="videogame-name">${videogame.name}</h3>
                                 <img src="${videogame.picture}" alt="Imagen del videojuego ${videogame.name}" class="videogame-image">
-                                <p>Año de lanzamiento: Año ${videogame.date}</p>
-                                <p>Nota: ${videogame.rating}</p>
-                                <p>Duración: ${videogame.duration}h</p>
-                                <p>Género: ${videogame.genre}</p>
+                                <p class="videogame-date"><span class="bold">Año de lanzamiento:</span> ${videogame.date}</p>
+                                <p class="videogame-rating"><span class="bold">Nota:</span> ${videogame.rating}</p>
+                                <p class="videogame-duration"><span class="bold">Duración:</span> ${videogame.duration}h</p>
+                                <p class="videogame-genre"><span class="bold">Género:</span> ${videogame.genre}</p>
                             </div>`;
         videogamesList.innerHTML += videogameCard;
     });
